ef8ad884f9221939ac6ae24d96d95744fe96d33c
golfurier <golfurier@hotmail.com>
2019-12-14 08:10:30 +0700

    first commit

---
+++ command
@@ -0,0 +1,8 @@
+$ django-admin startproject nobank
+$ python manage.py runserver
+$ python manage.py migrate
+$ python manage.py createsuperuser
+$ python manage.py check
+$ python manage.py startapp polls
+$ python manage.py makemigrations
+$ python manage.py shell
---
+++ nobank/nobank/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def home(request):
+    now = datetime.datetime.now()
+    context = {'datetime_now': now}
+    return render(request, 'home.html', context)
