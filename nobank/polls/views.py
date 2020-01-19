406d02f84fe20631156f1b31ba6263ca7f94c62a
golfurier <golfurier@hotmail.com>
2020-01-19 22:13:23 +0700

    ended of 43

--- nobank/polls/views.py
+++ nobank/polls/views.py
@@ -1,4 +1,4 @@
-from  django.urls import reverse
+from django.urls import reverse
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
